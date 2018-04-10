f9867f760e21607251cca9d6074fefba7f783673
alelax <alessandro.lausdei@gmail.com>
2018-04-10 17:04:05 +0200

    Sent messagage function added

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
    // alert("ciao");
+   $('.send-icon').click(function() {
+
+      //Recupera il messaggio dall'input
+      var $messageSent = $('#input-msg').val();
+      console.log($messageSent);
+
+      //Generea un nuovo messaggio, e lo inserisce alla fine della conversation-area
+      $('.conversation-area').append(
+         "<div class='message-row msg-sent'>"
+       +    "<div class='message-box'>"
+       +       $messageSent
+       +    "</div>"
+       + "</div>"  );
+
+      //Viene inserito un messaggio di risposta automatico "ok".
+      setTimeout(function(){
+         $('.conversation-area').append(
+            "<div class='message-row msg-received'>"
+          +    "<div class='message-box'>"
+          +       "ok"
+          +    "</div>"
+          + "</div>"  );
+      },1000);
+
+
+   })
+
+
 });
