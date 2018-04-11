694d077cbf5f57eb15d48f4598ed07dc69ec6de8
alelax <alessandro.lausdei@gmail.com>
2018-04-11 18:17:06 +0200

    Contacts search - updated

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ $(document).ready(function(){
    var newMessage;
    var now = new Date();
 
+   //Invio del messaggio tramite pressione del tasto Enter
    $('#input-msg').keypress(function(e) {
       //13 = Tasto invio
       if (e.which == 13) {
@@ -25,6 +26,7 @@ $(document).ready(function(){
       }
    });
 
+   //Invio del messaggio tramite click dell'icona di invio
    $('.send-icon').click(function() {
 
       var $thisInput = $('#input-msg');
@@ -38,35 +40,35 @@ $(document).ready(function(){
 
    });
 
-
+   //recupero l'input tramite il suo id
    var searchContacts = $('#contacts-search');
-   var digits = new Array();
-   $('#contacts-search').keydown(function(e) {
-      var newChar = e.key;
-      console.log(newChar);
-      if (newChar != "Backspace") {
-         digits.push(newChar);
-         console.log(digits);
-      } else {
-         digits.splice(-1, 1); //Il -1 permette di rimuovere l'ultimo elemento dell'array
-         console.log(digits);
-      }
 
+
+   $('#contacts-search').keyup(function(e) {
+
+      //recupero il contenuto dell'input
+      var instantInput = $(this).val().toLowerCase();
+
+      //recupero tutti gli elementi con classe 'chat-with'; essi contengono i nomi
+      //dei contatti. Su ognuno di essi viene effettuato il controllo per verificare
+      //se i caratteri immessi nell'input sono contenuti nel nome del contatto.
+      //Ogni contatto in base al risultato del controllo verrà mostrato o nascosto
       $('.chat-with').each(function(){
-         // console.log($(this).html());
-         var thisContact = $(this).parent().parent().parent();
-         console.log(thisContact);
-         var currentContact = $(this).html();
-         console.log(currentContact);
 
-         var isMatched = isCharachterMatched(currentContact, digits);
+         //Recupero il genitore del contatto, lo utilizzo per mostrare o noscondere il contatto
+         var thisContact = $(this).parent().parent().parent();
+
+         //Salvo in una variabile il nome del contatto corrente
+         var contactName = $(this).text().toLowerCase();
+
+         //Verifico che il contatto corrente contenga i caratteri digitati dall'utente
+         var isMatched = isCharachterMatched(contactName, instantInput);
 
          if (!isMatched) {
             thisContact.hide();
          } else {
             thisContact.show();
          }
-         // console.log(digits);
 
       });
 
@@ -75,15 +77,35 @@ $(document).ready(function(){
    });
 
 
-   function isCharachterMatched(nameToSearch, charArray) {
+   //Funzione che riceve il nome del contatto corrente (nameToSearch) e l'input immesso
+   //dall'utente
+   function isCharachterMatched(nameToSearch, instantInput) {
       var isCharsConteined = false;
       var noMatchCounter = 0;
 
-      for (var i = 0; i < charArray.length; i++) {
-         if ( !(nameToSearch.includes(charArray[i])) ) {
-            noMatchCounter++;
-         }
+
+      /*
+         Questo controllo è indipendente dalla sequenza con la quale vengono inseriti
+         i caratteri in input. Controlla che i caratteri siano contenuti nel nome senza
+         tener conto dell'ordine.
+         Il charAt all'interno del for permette di considerare e controllare ogni carattere
+         singolarmente
+      */
+
+      // for (var i = 0; i < instantInput.length; i++) {
+      //    if ( !(nameToSearch.includes(instantInput.charAt(i))) ) {
+      //       noMatchCounter++;
+      //    }
+      // }
+
+      //Controllo che il nome contenga i caratteri immessi dall'input. Se non li contiene
+      //il contatore aumenta
+      if ( !(nameToSearch.includes(instantInput)) ) {
+         noMatchCounter++;
       }
+
+      //Il contatore maggiore di 0 indica che il nome non contiene tutti i caratteri,
+      //perciò la variabile verrà impostata a false, altrimenti a true
       if (noMatchCounter > 0) {
       isCharsConteined = false;
       } else {
